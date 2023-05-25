2c682d510c2b026593f5e6bd6d9d20ecb3ff5ce2
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-05-25 23:06:03 +0200

    work in progres

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import ApiService from './js/api.js';
+
+const refs = {
+  searchForm: document.querySelector('.form'),
+  inputEL: document.querySelector('.form__input'),
+  addwatchedBtn: document.querySelector('.addwatched'),
+  addqueueBtn: document.querySelector('.addqueue'),
+  gallery: document.querySelector('.gallery'),
+};
+
+export const ApiService = new ApiService();
