ce5de32e59c17eae12c2d40ad01fa80721076726
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-05-26 16:34:44 +0200

    work in progress

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ApiService from './js/api.js';
 const refs = {
   searchForm: document.querySelector('.form'),
   inputEL: document.querySelector('.form__input'),
-  addwatchedBtn: document.querySelector('.addwatched'),
-  addqueueBtn: document.querySelector('.addqueue'),
+  addwatchedBtn: document.querySelector('.movieswatched'),
+  addqueueBtn: document.querySelector('.moviesqueue'),
   gallery: document.querySelector('.gallery'),
 };
 
 export const api = new ApiService();
 
 api.fetchDefault().then(data => {
-  console.log(data);
+  console.log(data.data.results);
 });
